3b7efdb7859e1f89f0863bf385b1d35de4ba97ff
Dominic Maas <dominic.maas@live.com>
2017-07-14 18:05:04 +1200

    Updated about page, fixed bugs.

--- SoundByte.UWP/Views/Application/AboutView.xaml.cs
+++ SoundByte.UWP/Views/Application/AboutView.xaml.cs
@@ -12,10 +12,10 @@ using System.IO;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Services.Store.Engagement;
 using Newtonsoft.Json;
 using SoundByte.Core.API.Endpoints;
 using SoundByte.UWP.Services;
-using SoundByte.UWP.Views.CoreApp;
 using SoundByte.UWP.Views.General;
 
 namespace SoundByte.UWP.Views.Application
@@ -35,25 +35,48 @@ namespace SoundByte.UWP.Views.Application
         {
             // Set the app version
             AppVersion.Text = $"Version: {Package.Current.Id.Version.Major}.{Package.Current.Id.Version.Minor}.{Package.Current.Id.Version.Build}.{Package.Current.Id.Version.Revision}";
-            AppBuildBranch.Text = "Build Branch: ...";
-            AppBuildVersion.Text = "Build Number: ...";
-            AppBuildTime.Text = "Build Time: ...";
+            AppBuildBranch.Text = "...";
+            AppBuildTime.Text = "...";
 
             var dataFile = await Package.Current.InstalledLocation.GetFileAsync(@"Assets\build_info.json");
             var buildData = await Task.Run(() => JsonConvert.DeserializeObject<BuildInfo>(File.ReadAllText(dataFile.Path)));
 
-            AppBuildBranch.Text = "Build Branch: " + buildData.BuildBranch;
-            AppBuildVersion.Text = "Build Number: " + buildData.BuildNumber;
-            AppBuildTime.Text = "Build Time: " + buildData.BuildTime;
+            AppBuildBranch.Text = buildData.BuildBranch;
+            AppBuildTime.Text = buildData.BuildTime;
 
             TelemetryService.Current.TrackPage("About Page");
         }
 
-        public void NavigateWelcome()
+        public async void NavigateBugs()
         {
-            App.NavigateTo(typeof(WelcomeView));
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://gridentertainment.net/fwlink/GvC5iXmJSo"));
         }
 
+        public async void NavigateFeedback()
+        {
+           var launcher = StoreServicesFeedbackLauncher.GetDefault();
+           await launcher.LaunchAsync();
+        }
+
+        public async void NavigatePrivacy()
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://gridentertainment.net/fwlink/Y5jGLtoFXs"));
+        }
+
+        public async void NavigateReddit()
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://gridentertainment.net/fwlink/68vfoKLYJS"));
+        }
+
+        public async void NavigateFacebook()
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://gridentertainment.net/fwlink/rOye5hzCXt"));
+        }
+
+        public async void NavigateGitHub()
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://gridentertainment.net/fwlink/O3i37tbVVO"));
+        }
         public void NavigateNew()
         {
             App.NavigateTo(typeof(WhatsNewView));
